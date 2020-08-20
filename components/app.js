785f2f04bfc7d6b8c62dd0e065cacb86fa9adbe5
donark1 <don_ark@sbcglobal.net>
2020-08-19 18:03:50 -0700

    View All Grades App.js creation

--- components/app.js
+++ components/app.js
@@ -1,3 +1,27 @@
 class App {
-  handleGetGradesError
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  constructor() {
+    this.handleGetGradesError = handleGetGradesError;
+    this.handleGetGradesSuccess = handleGetGradesSuccess;
+  }
+  getGrades() {
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "GET",
+      data: "none",
+      headers: {
+        "X-Access-Token": "bykh0Jvl"
+      },
+      success: this.handleGetGradesSuccess();
+      error: this.handleGetGradesError();
+    });
+  }
+  start() {
+    this.getGrades(),
+  }
 }
